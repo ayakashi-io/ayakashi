ffad98d5160c8fd3518789909bd809b99ef1ee77
Zisis Maras <contact@zisismaras.me>
2019-05-31 18:28:12 +0300

    add new stream-based chromium downloader/extractor

--- src/chromeDownloader/downloader.ts
+++ src/chromeDownloader/downloader.ts
@@ -1,9 +1,9 @@
 import request from "request";
-import decompress from "decompress";
 import {getOpLog} from "../opLog/opLog";
-import {lstatSync} from "fs";
+import {lstatSync, createWriteStream, createReadStream, unlink, chmodSync} from "fs";
 import rimraf from "rimraf";
 import {resolve as pathResolve} from "path";
+import unzipper from "unzipper";
 
 export function isChromiumAlreadyInstalled(projectFolder: string) {
     try {
@@ -31,7 +31,14 @@ export function cleanChromiumDirectory(projectFolder: string) {
             resolve();
         }
     });
+}
 
+function cleanZipFile(zipPath: string) {
+    return new Promise(function(resolve) {
+        unlink(zipPath, function(_err) {
+            resolve();
+        });
+    });
 }
 
 export function downloadLatestChromium(projectFolder: string) {
@@ -64,28 +71,42 @@ export function downloadLatestChromium(projectFolder: string) {
         let total = "0mb";
         let downloaded = 0;
         const waiter = opLog.waiter("0.0MB/0.0MB");
-        const buffs: Buffer[] = [];
         downloadStream.on("response", function(resp) {
             total = toMb(parseInt(resp.headers["content-length"] || "0"));
         });
+        const fileStream = createWriteStream(`${projectFolder}/.chromium.zip`);
+        downloadStream.pipe(fileStream);
         downloadStream.on("data", function(chunk) {
             downloaded += chunk.length;
-            if (typeof chunk === "string") {
-                buffs.push(Buffer.from(chunk));
-            } else {
-                buffs.push(chunk);
-            }
             waiter.text = `${toMb(downloaded)}/${total}`;
         });
         downloadStream.on("end", function() {
             waiter.text = "extracting...";
-            decompress(Buffer.concat(buffs), `${projectFolder}/.chromium`, {strip: 1}).then(function() {
+            const zipStream = createReadStream(`${projectFolder}/.chromium.zip`);
+            const unzipStream = unzipper.Extract({path: `${projectFolder}/.chromium`});
+            zipStream.pipe(unzipStream);
+            zipStream.on("error", async function(err) {
+                waiter.fail(err.message);
+                await cleanZipFile(`${projectFolder}/.chromium.zip`);
+                reject(err);
+            });
+            unzipStream.on("error", async function(err) {
+                waiter.fail(err.message);
+                await cleanZipFile(`${projectFolder}/.chromium.zip`);
+                await cleanChromiumDirectory(projectFolder);
+                reject(err);
+            });
+            unzipStream.on("finish", async function() {
                 waiter.succeed("done!");
+                await cleanZipFile(`${projectFolder}/.chromium.zip`);
+                const chromePath = getChromePath(projectFolder);
+                chmodSync(chromePath,  0o755);
                 resolve();
             });
         });
-        downloadStream.on("error", function(err) {
+        downloadStream.on("error", async function(err) {
             waiter.fail(err.message);
+            await cleanZipFile(`${projectFolder}/.chromium.zip`);
             reject(err);
         });
     });
@@ -93,16 +114,20 @@ export function downloadLatestChromium(projectFolder: string) {
 
 export function getChromePath(projectFolder: string) {
     let executable = "";
+    let subfolder = "";
     if (process.platform === "linux") {
         executable = "chrome";
+        subfolder = "chrome-linux";
     } else if (process.platform === "darwin") {
         executable = "Chromium.app/Contents/MacOS/Chromium";
+        subfolder = "chrome-mac";
     } else if (process.platform === "win32") {
         executable = "chrome.exe";
+        subfolder = "chrome-win";
     } else {
         throw new Error("invalid_platform");
     }
-    return pathResolve(projectFolder, ".chromium", executable);
+    return pathResolve(projectFolder, ".chromium", subfolder, executable);
 }
 
 function toMb(bytes: number) {
