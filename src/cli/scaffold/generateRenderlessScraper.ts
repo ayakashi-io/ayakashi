8d4d6edc5206e370c06bba0c18faf2e405d0bb12
Zisis Maras <contact@zisismaras.me>
2019-08-15 19:09:19 +0300

    update scaffold examples

--- src/cli/scaffold/generateRenderlessScraper.ts
+++ src/cli/scaffold/generateRenderlessScraper.ts
@@ -39,10 +39,20 @@ function getContent() {
 module.exports = async function(ayakashi, input, params) {
     await ayakashi.load(input.page);
     ayakashi
+        .select("name")
+        .where({itemprop: {eq: "name"}});
+    ayakashi
+        .select("author")
+        .where({itemprop: {eq: "author"}});
+    ayakashi
         .select("about")
         .where({itemprop: {eq: "about"}});
 
-    return ayakashi.extract("about");
+    return {
+        name: await ayakashi.extractFirst("name"),
+        author: await ayakashi.extractFirst("author"),
+        about: await ayakashi.extractFirst("about")
+    };
 };
 `);
 }
