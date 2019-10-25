ab0271db065ec545c38d1fac4294cba19606ff82
Zisis Maras <contact@zisismaras.me>
2019-10-25 21:05:26 +0300

    add typedocs

--- src/prelude/actions/cookies.ts
+++ src/prelude/actions/cookies.ts
@@ -44,12 +44,70 @@ type NewCookie = {
 };
 
 export interface ICookieActions {
+/**
+ * Get a single cookie that passes the filter.
+ * Learn more here: https://ayakashi.io/docs/going_deeper/manipulating-cookies.html#manual-cookie-manipulation
+ * ```js
+const cookie = await ayakashi.getCookie({ //filter object
+    key: "myKey" //optional,
+    domain: "somepage.com" //optional,
+    path: "/test" //optional,
+    url: "https://somepage.com" //optional
+});
+```
+*/
     getCookie: (filter?: CookieFilter) => Promise<SerializedCookie | null>;
-
+/**
+ * Get multiple cookies that pass the filter.
+ * Learn more here: https://ayakashi.io/docs/going_deeper/manipulating-cookies.html#manual-cookie-manipulation
+ * ```js
+const cookies = await ayakashi.getCookies({ //filter object
+    key: "myKey" //optional,
+    domain: "somepage.com" //optional,
+    path: "/test" //optional,
+    url: "https://somepage.com" //optional
+});
+```
+*/
     getCookies: (filter?: CookieFilter) => Promise<SerializedCookie[]>;
-
+/**
+ * Set a single cookie.
+ * Learn more here: https://ayakashi.io/docs/going_deeper/manipulating-cookies.html#manual-cookie-manipulation
+ * ```js
+await ayakashi.setCookie({
+    key: "myCookie",
+    value: "test",
+    domain: "somepage.com",
+    path: "/",
+    expires: "2029-10-25T17:29:23.375Z", //optional
+    secure: true, //optional
+    httpOnly: true, //optional
+    hostOnly: false //optional
+});
+```
+*/
     setCookie: (cookie: NewCookie) => Promise<void>;
-
+/**
+ * Set multiple cookies.
+ * Learn more here: https://ayakashi.io/docs/going_deeper/manipulating-cookies.html#manual-cookie-manipulation
+ * ```js
+await ayakashi.setCookies([{
+    key: "myCookie",
+    value: "test",
+    domain: "somepage.com",
+    path: "/",
+    expires: "2029-10-25T17:29:23.375Z", //optional
+    secure: true, //optional
+    httpOnly: true, //optional
+    hostOnly: false //optional
+}, {
+    key: "myCookie2",
+    value: "someValue",
+    domain: "anotherpage.com",
+    path: "/"
+}]);
+```
+*/
     setCookies: (cookies: NewCookie[]) => Promise<void>;
 }
 
