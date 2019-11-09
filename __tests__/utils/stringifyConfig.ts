1d94ec4612da59fd40f1a2c53f56bcb06df88414
Zisis Maras <contact@zisismaras.me>
2019-11-09 13:57:31 +0200

    add support for base64 encoded jsonConfig and use it for e2e tests

--- __tests__/utils/stringifyConfig.ts
+++ __tests__/utils/stringifyConfig.ts
@@ -1,12 +1,6 @@
 import {Config} from "../../src/runner/parseConfig";
 
 export function stringifyConfig(config: Config): string {
-    let strConfig: string;
-    if (process.platform === "win32") {
-        strConfig = JSON.stringify(config).replace(/"/g, `\\"`);
-    } else {
-        strConfig = JSON.stringify(config);
-    }
-
-    return strConfig;
+    const strConfig = JSON.stringify(config);
+    return Buffer.from(strConfig.toString()).toString("base64");
 }
--- src/cli/prepareFromJson.ts
+++ src/cli/prepareFromJson.ts
@@ -9,11 +9,15 @@ export function prepareFromJson(dir: string, jsonConfig: string) {
     opLog.info("configFile: from json");
     try {
         return {
-            config: JSON.parse(jsonConfig),
+            config: JSON.parse(isBase64(jsonConfig) ? Buffer.from(jsonConfig, "base64").toString("utf8") : jsonConfig),
             directory: directory
         };
     } catch (_e) {
         opLog.error("Invalid json passed to --jsonConfig");
         return process.exit(1);
     }
+}
+
+function isBase64(str: string) {
+    return Buffer.from(str, "base64").toString("base64") === str;
 }
